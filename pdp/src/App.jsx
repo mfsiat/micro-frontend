c1c4b80f5f345c02220b54ae59784145ea09485a
Md. Nasirul Islam <nasirul.islam@northsouth.edu>
2021-11-21 02:38:51 +0600

    update diagram & update code

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,15 +1,17 @@
 import Footer from 'home/Footer';
 import Header from 'home/Header';
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 
-
+const Header = React.lazy(() => import ("home/Header"));
 
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <Suspense>
     <Header />
+    </Suspense>
     <div className="my-10">
       PDP Page
     </div>
